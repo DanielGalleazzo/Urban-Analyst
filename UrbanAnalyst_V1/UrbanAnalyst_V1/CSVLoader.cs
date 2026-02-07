f69635f23e698dc1ac92279490a6b7f6696c1b3f
Daniel Augusto Galleazzo <danielgalleazzo@gmail.com>
2026-02-07 19:17:10 -0300

    Iniciando o projeto/testando abrir um arquivo csv

---
+++ UrbanAnalyst_V1/UrbanAnalyst_V1/CSVLoader.cs
@@ -0,0 +1,30 @@
+﻿using UrbanMobilityAnalyzer.Models;
+
+namespace UrbanMobilityAnalyzer.Services;
+
+public class CsvLoader
+{
+    public List<GpsPoint> Load(string path)
+    {
+        var points = new List<GpsPoint>();
+
+        var lines = File.ReadAllLines(path);
+
+
+        for (int i = 1; i < lines.Length; i++)
+        {
+            var cols = lines[i].Split(',');
+
+            var point = new GpsPoint
+            {
+                Timestamp = DateTime.Parse(cols[0]),
+                Latitude = double.Parse(cols[1]),
+                Longitude = double.Parse(cols[2])
+            };
+
+            points.Add(point);
+        }
+
+        return points;
+    }
+}
---
+++ UrbanAnalyst_V1/UrbanAnalyst_V1/Program.cs
@@ -0,0 +1,12 @@
+﻿using UrbanMobilityAnalyzer.Services;
+
+var loader = new CsvLoader();
+
+var points = loader.Load("C:\\Users\\danie\\Desktop\\TesteCSV.csv");
+
+Console.WriteLine($"Pontos carregados: {points.Count}");
+
+foreach (var p in points.Take(3))
+{
+    Console.WriteLine($"{p.Timestamp} | {p.Latitude} | {p.Longitude}");
+}
